91c34587c7ee4e7e21b8af3dcebfb803b3eb47c1
Anastasia Saiz <71260261+AnastasiaSaiz@users.noreply.github.com>
2020-12-09 17:49:40 +0100

    modif

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from "react";
 
 function App() {
+
+  const [num, setNum] = useState(0);
+
+  function click() {
+    setNum(num + 1)
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>{num}</h1>
+      <button onClick={click}> click</button>
     </div>
-  );
+  )
+
 }
 
 export default App;
